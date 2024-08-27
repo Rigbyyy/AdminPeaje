4f100ff0307403be54efb97e6b5667c4d3791360
Rigbyyy <cazunigaca@est.utn.ac.cr>
2024-08-27 16:33:38 -0600

    Clase Vehiculo

---
+++ Vehiculos/Vehiculo.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Vehiculos;
+
+/**
+ *
+ * @author ASUS
+ */
+public abstract class Vehiculo {
+ private String  matricula;
+ private String marca;
+ private String modelo;
+private int  año;
+
+
+public abstract double calcularTarifa();
+    
+
+    
+}
